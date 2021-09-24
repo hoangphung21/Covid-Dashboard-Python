8147b0c2aaeb66f94cfbdad875b3c2355f66b7c1
Hoang Phung <phunghoang211195@gmail.com>
2021-09-24 14:06:02 -0400

    created layout

--- index.py
+++ index.py
@@ -2,6 +2,7 @@ import dash
 from dash import html
 from dash import dcc
 from dash.dependencies import Input, Output
+from dash.html.H1 import H1
 import plotly.graph_objects as go
 import pandas as pd
 
@@ -13,7 +14,53 @@ confirmed = pd.read_csv(url_confirmed)
 deaths = pd.read_csv(url_deaths)
 recovered = pd.read_csv(url_recovered)
 
+# unpivot data
+date1 = confirmed.columns[4:]
+total_confirmed = confirmed.melt(id_vars=['Province/State','Country/Region','Lat','Long'],
+    value_vars=date1,
+    var_name='date',
+    value_name='confirmed')
 
+date2 = deaths.columns[4:]
+total_deaths = deaths.melt(id_vars=['Province/State','Country/Region','Lat','Long'],
+    value_vars=date2,
+    var_name='date',
+    value_name='deaths')
 
+date3 = recovered.columns[4:]
+total_recovered = recovered.melt(id_vars=['Province/State','Country/Region','Lat','Long'],
+    value_vars=date2,
+    var_name='date',
+    value_name='recovered')
 
+# merging data frames
+covid_data = total_confirmed.merge(right = total_deaths,
+    how='left',
+    on=['Province/State','Country/Region','Lat','Long','date'],
+    )
+covid_data = covid_data.merge(right = total_recovered,
+    how='left',
+    on=['Province/State','Country/Region','Lat','Long','date'],
+    )
 
+# converting date format from string on date column
+covid_data['date'] = pd.to_datetime(covid_data['date'])
+
+# checking sum of missing values
+covid_data.isna().sum()
+
+# replacing with zero
+covid_data['recovered'] = covid_data['recovered'].fillna(0)
+
+# adding new column
+covid_data['active'] = covid_data['confirmed'] - covid_data['deaths'] - covid_data['recovered']
+
+# creating layout of the dash app
+app = dash.Dash(__name__, )
+
+app.layout = html.Div([
+    
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
